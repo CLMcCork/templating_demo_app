c8a92c1319f70f3fedf31d048e4e58873a385d11
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-05-21 19:20:41 -0500

    added logic to index.js file
    Also added random.ejs file

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
+app.get("/rand", (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1; 
+    //goal is to move as much logic out of .ejs files and put it in here instead
+    res.render("random", { rand: num })
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!")
 })
